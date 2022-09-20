1f66c287e989e61eb403d61c4dbf20e9989d6b32
zeddy cherotich <107035298+zeddymuge@users.noreply.github.com>
2022-09-20 16:19:15 +0300

    Task done

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 def fizzbuzz():
-  for i in range(1,101):
+    for i in range(1, 101):
         if i % 3 == 0:
-            print("fizz", end =" ")
-        elif i % 5 == 0:
-            print("buzz" ,end = " ")
-        elif i % 3 and 1 % 5 == 0:
-            print("fizzbuzz" ,end = " ")
-        else:
-            print(i," ", end = "")
+            print("Fizz", end='')
+        if i % 5 == 0:
+            print("Buzz", end='')
+        if i % 3 and i % 5:
+            print("{:d}".format(i), end='')
+        print(end=' ')
