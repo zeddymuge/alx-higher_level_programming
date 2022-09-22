73f3d303512e7df2e75fcc477a661694f8312fab
zeddy cherotich <107035298+zeddymuge@users.noreply.github.com>
2022-09-22 13:30:17 +0300

    done

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 import sys
 if __name__ == "__main__":
-  sum = 0
-  if len(sys.argv) > 1:
-    for z in range(1, len(sys.argv)):
-      sum += int(sys.argv[z])
-  print("{}".format(sum)) 
+    size = len(sys.argv)
+    sum = 0
+    if size > 1:
+        for z in range(1, size):
+            sum += int(sys.argv[z])
+    print("{}".format(sum))
