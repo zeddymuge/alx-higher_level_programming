ef6fa5092eb9e44ccd4860f61a90900b537b6284
zeddy cherotich <107035298+zeddymuge@users.noreply.github.com>
2022-12-20 09:11:03 +0300

    checking

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,18 +1,15 @@
 #!/usr/bin/python3
 """
-python script that lists all states from the database hbtn_0e_0_usa
+This module lists all states from the database hbtn_0e_0_usa
 """
-
-import MySQLdb
-from sys import argv
-
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-                         passwd=argv[2], db=argv[3], charset="utf8")
-    cursor = db.cursor()
-    cursor.execute("SELECT * FROM states ORDER BY id ASC")
-    rows = cursor.fetchall()
+    import sys
+    import MySQLdb
+
+    db = MySQLdb.connect(host='localhost', user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states ORDER BY states.id")
+    rows = cur.fetchall()
     for row in rows:
         print(row)
-    cursor.close()
-    db.close()
